eabd6a04830af0510f14675fbfd3750bb1035861
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-04-06 22:50:41 +0300

    Add project files.

---
+++ MyWeb/Controllers/MyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWeb.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet(Name = "GetMyController")]
+   
+        public string SayHello(string name)
+        {
+            return $"Hello {name}";
+        }
+
+    }
+}
+
