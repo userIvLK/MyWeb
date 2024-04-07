00d25d84b592a9e8a3ab5acced619ea2f2da9bc0
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-04-07 21:38:06 +0300

    Solved the task

--- MyWeb/Controllers/MyController.cs
+++ MyWeb/Controllers/MyController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MyWeb.Models;
 namespace MyWeb.Controllers
 
 {
@@ -8,12 +8,27 @@ namespace MyWeb.Controllers
     public class MyController : ControllerBase
     {
         [HttpGet(Name = "GetMyController")]
-   
+
         public string SayHello(string name)
         {
             return $"Hello {name}";
         }
 
+        [HttpPost(Name = "PostMyController")]
+
+        public OrderItem Somefunc(Order order)
+        {
+            List<OrderItem> items = order.Items;
+            int minPriceIndex = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].Price < items[minPriceIndex].Price)
+                {
+                    minPriceIndex= i;
+                }
+            }
+            return items[minPriceIndex];
+        }
     }
 }
 
