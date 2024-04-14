82a742cacdb77ec7e48a93c872b12212a4c4deea
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-04-14 23:41:04 +0300

    Changed funcionality

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -14,9 +14,9 @@ namespace MyWeb
                 using (var httpClient = new HttpClient())
                 {
                     httpClient.BaseAddress = new Uri("https://localhost:7057/My");
-                    OrderItem o1 = new() { Id = Guid.NewGuid(), Price = 90, Quantity = 1, Name = "A" };
-                    OrderItem o2 = new() { Id = Guid.NewGuid(), Price = 10, Quantity = 1, Name = "B" };
-                    OrderItem o3 = new() { Id = Guid.NewGuid(), Price = 20, Quantity = 1, Name = "c" };
+                    OrderItem o1 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "A" };
+                    OrderItem o2 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "B" };
+                    OrderItem o3 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "c" };
                     List<OrderItem> orderList = new() { o1, o2, o3 };
                     Order myOrder = new() { Id = Guid.NewGuid(), Customer = "Ivan", Items = orderList, OrderDate = DateTime.Now};
                     using HttpResponseMessage response = await httpClient.PostAsJsonAsync(httpClient.BaseAddress.AbsoluteUri, myOrder);
