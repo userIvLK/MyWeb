4c3bd3ace750ed752782ed01a54261a0df9707ea
userIvLK <121995211+userIvLK@users.noreply.github.com>
2024-04-16 15:00:23 +0300

    Changed the program

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -7,22 +7,28 @@ namespace MyWeb
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            for(int i = 0; i < 1000; ++i)
+            using (var httpClient = new HttpClient())
             {
-                using (var httpClient = new HttpClient())
+                httpClient.BaseAddress = new Uri("https://localhost:7057/My?name=abc");
+                var response = httpClient.GetAsync(httpClient.BaseAddress.AbsoluteUri).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result);
+            }
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://localhost:7057/My");
+                OrderItem o1 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "A" };
+                OrderItem o2 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "B" };
+                OrderItem o3 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "c" };
+                List<OrderItem> orderList = new() { o1, o2, o3 };
+                Order myOrder = new() { Id = Guid.NewGuid(), Customer = "Ivan", Items = orderList, OrderDate = DateTime.Now };
+                var response = httpClient.PostAsJsonAsync(httpClient.BaseAddress.AbsoluteUri, myOrder).Result;
+                var result = response.Content.ReadFromJsonAsync<OrderItem>().Result;
+                if(result != null)
                 {
-                    httpClient.BaseAddress = new Uri("https://localhost:7057/My");
-                    OrderItem o1 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "A" };
-                    OrderItem o2 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "B" };
-                    OrderItem o3 = new() { Id = Guid.NewGuid(), Price = new Random().Next(1, 100), Quantity = 1, Name = "c" };
-                    List<OrderItem> orderList = new() { o1, o2, o3 };
-                    Order myOrder = new() { Id = Guid.NewGuid(), Customer = "Ivan", Items = orderList, OrderDate = DateTime.Now};
-                    using HttpResponseMessage response = await httpClient.PostAsJsonAsync(httpClient.BaseAddress.AbsoluteUri, myOrder);
-                    var result = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(result);
-
+                    Console.Write($"Name: {result.Name}, Price: {result.Price}"); 
                 }
             }
         }
